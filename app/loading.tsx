1928e0e9bf3358244fc80af2006b0d98ac5c031c
Ishwar Singh <ishwar.singh@neosoftmail.com>
2025-02-14 01:52:11 +0530

    fix minor issue, cleaned up rootlayout

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ const Loading = () => {
     return (
         <div className="absolute inset-0 bg-black/25 grid place-content-center">
             <p className="sr-only">Loading...</p>
-            <div className="size-25 border-4 border-black border-t-0 rounded-full"></div>
+            <div className="size-25 border-6 border-black/90 border-t-blue-100 animate-spin rounded-full"></div>
         </div>
     )
 }
